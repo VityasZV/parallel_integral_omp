91647c977230677925837bb37d29edd62b121c0e
Виктор Задябин <viktor.zadyabin@mail.ru>
2019-10-21 19:40:22 +0300

    omp variant works

--- parallel_integral_openmp/parallel_integral_omp.cpp
+++ parallel_integral_openmp/parallel_integral_omp.cpp
@@ -1,10 +1,8 @@
 #include "parallel_integral_omp.hpp"
 
 
-#include <future>
 #include <iostream>
 #include <cmath>
-#include <vector>
 
 namespace parallel_integral {
 
@@ -12,14 +10,13 @@ namespace parallel_integral {
 
 		static const double kAccuracy = 0.00001;
 
-		static const size_t t_size = std::thread::hardware_concurrency();
 
 		double Function(const double& arg) {
-			return arg;
+			return 3 * std::pow(arg, 2) + 2 * arg;
 		}
 
 		struct AccuracyParameters {
-			double parts = 2;
+			int parts = 2;
 			double step;
 			const Limits limits;
 			AccuracyParameters(std::ifstream& input_file): limits(input_file){
@@ -38,7 +35,6 @@ namespace parallel_integral {
 
 	Limits::Limits(std::ifstream& input_file) {
 		input_file.open("input.txt");
-		std::string l, r;
 		input_file >> left >> right;
 		input_file.close();
 	}
@@ -47,14 +43,19 @@ namespace parallel_integral {
 		std::ifstream input_file;
 		AccuracyParameters accuracy_parameters(input_file);
 		double previous_result = 0, result = 0;
+		int i = 0;
 		do {
+			
 			previous_result = result;
 			result = 0;
 			accuracy_parameters.Increment();
-#pragma omp parallel
-			for (auto i = accuracy_parameters.limits.left; i < accuracy_parameters.limits.right; i += accuracy_parameters.step){
-#pragma omp atomic
-				result += accuracy_parameters.step * Function(i + accuracy_parameters.step / 2);
+#pragma omp parallel shared(accuracy_parameters) reduction(+:result)
+			{
+				#pragma omp for
+				for (i = 0; i < accuracy_parameters.parts; ++i) {
+					auto x = accuracy_parameters.limits.left + i * accuracy_parameters.step;
+					result += accuracy_parameters.step * Function(x + accuracy_parameters.step / 2);
+				}
 			}
 
 		} while (abs(result - previous_result) >= kAccuracy);
